9014120406d04c722a8f0aaea389a0eb4a182b54
WafaBergaoui <wafabergaoui3@gmail.com>
2021-03-15 16:44:59 +0100

    third commit

--- firebase/config.js
+++ firebase/config.js
@@ -1,11 +1,21 @@
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+} from "@env";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDOQ4gFyIPJqwnH5fJselRcMZRA6-hG3dw",
-  authDomain: "jamah-e6e58.firebaseapp.com",
-  databaseURL: "https://jamah-e6e58.firebaseio.com/",
-  projectId: "jamah-e6e58",
-  storageBucket: "jamah-e6e58.appspot.com",
-  messagingSenderId: "926800474230",
-  appId: "1:926800474230:web:ddba105bb5b03a71f5af57",
+  apiKey: `${API_KEY}`,
+  authDomain: `${AUTH_DOMAIN}`,
+  databaseURL: `${DATABASE_URL}`,
+  projectId: `${PROJECT_ID}`,
+  storageBucket: `${STORAGE_BUCKET}`,
+  messagingSenderId: `${MESSAGING_SENDER_ID}`,
+  appId: `${APP_ID}`,
 };
 
 export default firebaseConfig;
